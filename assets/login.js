38089d8d8b9a0c2eb8b925c95039a1f140b74784
Dan <dan.sasaki12@gmail.com>
2018-03-13 19:38:04 -0600

    adding firebase

---
+++ assets/login.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDdKJdKWyBww3paQE--cB_cF8SPa-FJn3c",
+authDomain: "kitchenkit-bf8b5.firebaseapp.com",
+databaseURL: "https://kitchenkit-bf8b5.firebaseio.com",
+projectId: "kitchenkit-bf8b5",
+storageBucket: "kitchenkit-bf8b5.appspot.com",
+messagingSenderId: "44047573836"
+};
+firebase.initializeApp(config);
