9f276e3b6ec317f8ba2e3d96de5fb8d7567637e8
Dan <dan.sasaki12@gmail.com>
2018-03-13 20:20:44 -0600

    adding firebase/auth

--- assets/login.js
+++ assets/login.js
@@ -40,10 +40,7 @@ var uiConfig = {
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     tosUrl: '<your-tos-url>'
@@ -68,9 +65,7 @@ var uiConfig = {
         clientId: 'xxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
       },
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Required to enable one-tap sign-up credential helper.
     credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
