65023994ca424b1ff56b8e1c63fb82b6d0777176
Dan <dan.sasaki12@gmail.com>
2018-03-17 11:26:33 -0600

    update

--- assets/login.js
+++ assets/login.js
@@ -15,7 +15,6 @@ var uiConfig = {
     signInSuccessUrl: "mainPage.html",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
@@ -27,34 +26,11 @@ var uiConfig = {
   // The start method will wait until the DOM is loaded.
   ui.start('#firebaseui-auth-container', uiConfig);
 
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        // Google provider must be enabled in Firebase Console to support one-tap
-        // sign-up.
-        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // Required to enable this provider in one-tap sign-up.
-        authMethod: 'https://accounts.google.com',
-        // Required to enable ID token credentials for this provider.
-        // This can be obtained from the Credentials page of the Google APIs
-        // console.
-        clientId: 'xxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
-      },
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Required to enable one-tap sign-up credential helper.
-    credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
-  });
-  // Auto sign-in for returning users is enabled by default except when prompt is
-  // not 'none' in the Google provider custom parameters. To manually disable:
-  ui.disableAutoSignIn();
 
   //Auth Check & User Log
-
-
-   firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function(user) {
      
-     user = user;
+     user == user;
 
      console.log('user', 'authStateChanged', user);
    });
